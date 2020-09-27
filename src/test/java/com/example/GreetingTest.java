a896db3b32fe1ab81857766e4e67a1a774577605
nagalr <nagalr@gmail.com>
2020-09-27 12:11:48 +0300

    Initial commit

---
+++ src/test/java/com/example/GreetingTest.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GreetingTest {
+
+    Greeting greeting;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("In before all");
+    }
+
+    @BeforeEach
+    void setUp() {
+        greeting = new Greeting();
+        System.out.println("In before each");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In after each");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("In after all");
+    }
+
+    @Test
+    void helloWorld() {
+        System.out.println(greeting.helloWorld());
+    }
+
+    @Test
+    void testHelloWorld() {
+        System.out.println(greeting.helloWorld("Queen"));
+    }
+}
